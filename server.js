e48ef9e52ae0143d8d1e87614e9372ac95d19db6
whaberer <91747264+willhaberer@users.noreply.github.com>
2021-11-16 19:09:43 -0500

    added get request for homepage

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
 const express = require("express");
-
+const path = require("path");
 const PORT = process.env.port || 3001;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`);
+});
